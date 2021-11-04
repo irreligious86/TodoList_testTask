db2a34f89de1094f9bece9920183e03b48d50cb5
Alexxs <irreligious86@gmail.com>
2021-11-04 09:40:04 +0100

    complete render function  with  interpolation

--- script.js
+++ script.js
@@ -1,6 +1,72 @@
 const inputAddTask = document.querySelector('.input-add-task');
 const buttonAddTask = document.querySelector('.button-add-task');
+const tasksList = document.querySelector('.tasks-list');
 
+let todoListArr = [
+    {
+        todo: "I am task list item",
+        checked: false,
+        important: false
+    },
+    {
+        todo: "I am important task list item",
+        checked: false,
+        important: true
+    },
+    {
+        todo: "I am checked task list item",
+        checked: true,
+        important: false
+    },
+    {
+        todo: "I am checked important task list item",
+        checked: true,
+        important: true
+    }
+];
 
-console.log(inputAddTask);
-console.log(buttonAddTask);
+const buttonAddTaskHandler = () => {
+    let newTodo = {
+        todo: inputAddTask.value,
+        checked: false,
+        important: false
+    }
+    if (!inputAddTask.value) {
+        alert("Task cannot be empty!");
+        return;
+    }
+    if (inputAddTask.value[0] === ' ') {
+        alert("Task value must start with non-SPACE symbol!");
+        inputAddTask.value = '';
+        return;
+    }
+    todoListArr.push(newTodo);
+    renderMessages();
+    inputAddTask.value = '';
+}
+
+const renderMessages = () => {
+    let renderMessage = '';
+    todoListArr.forEach((item, i) => {
+        renderMessage += `
+            <li class=" ${item.checked ? ' checked' : ''}">
+                <input 
+                
+                type="checkbox" 
+                id="item_${++i}" 
+                ${item.checked ? 'checked' : ''}
+                >
+                <label
+                 class="tasks-list-item ${item.important ? ' important' : ''}"
+                 for="item_${++i}"
+                 
+                 >${item.todo}</label>
+            </li>
+     `;
+        tasksList.innerHTML = renderMessage;
+    })
+}
+
+renderMessages();
+
+buttonAddTask.addEventListener('click', buttonAddTaskHandler);
