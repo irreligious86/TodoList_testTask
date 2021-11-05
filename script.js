2185c5ab2efb2dd92f0c672c26add2f799831233
Alexxs <irreligious86@gmail.com>
2021-11-05 12:47:02 +0100

    complete functions for invert importance and checked

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const inputAddTask = document.querySelector('.input-add-task');
 const buttonAddTask = document.querySelector('.button-add-task');
 const tasksList = document.querySelector('.tasks-list');
-
 let todoListArr = [
     {
         todo: "I am task list item",
@@ -25,48 +24,120 @@ let todoListArr = [
     }
 ];
 
+if (localStorage.getItem('todo')) {
+    todoListArr = JSON.parse(localStorage.getItem('todo'))
+} else {
+    localStorage.setItem('todo', JSON.stringify(todoListArr));
+}
+
+const renderSingleTask = (task, index) => {
+    const newItem = document.createElement('li');
+    const newItemLabel = document.createElement('label');
+    const newCheckbox = document.createElement('input');
+    const removeBtn = document.createElement('button');
+    const removeBtnText = document.createTextNode('X');
+
+    const renderNewItem = () => {
+        newItem.classList.add('tasks-list-item');
+        newItem.important = task.important;
+        newItemLabel.innerHTML = task.todo;
+        newItem.append(newItemLabel);
+        newItem.appendChild(removeBtn);
+        newItem.prepend(newCheckbox);
+
+        if (newItem.important) {
+            newItem.classList.add('important');
+        }
+    };
+
+    const renderNewCheckbox = () => {
+        newCheckbox.type = 'checkbox';
+        newCheckbox.id = +index;
+        newItemLabel.htmlFor = newCheckbox.id;
+        newCheckbox.checked = task.checked;
+        newCheckbox.classList.add('tasks-list-item');
+        newCheckbox.onchange = (() => {
+            if (newCheckbox.checked) {
+                newCheckbox.classList.add('checked');
+            } else {
+                newCheckbox.classList.remove('checked');
+            }
+        })
+    };
+
+    const renderNewRemoveBtn = () => {
+        removeBtn.type = 'button';
+        removeBtn.classList.add('remove-btn', 'righted');
+        removeBtn.append(removeBtnText);
+    };
+
+    tasksList.appendChild(newItem);
+    renderNewItem();
+    renderNewCheckbox();
+    renderNewRemoveBtn();
+};
+
+const renderTasks = () => todoListArr.forEach((item, index) => renderSingleTask(item, index));
+
+const reloadRender = () => {
+    while (tasksList.hasChildNodes()) {
+        tasksList.removeChild(tasksList.firstChild)
+    }
+    inputAddTask.value = '';
+    renderTasks();
+};
+
 const buttonAddTaskHandler = () => {
+
     let newTodo = {
         todo: inputAddTask.value,
         checked: false,
         important: false
     }
+
     if (!inputAddTask.value) {
         alert("Task cannot be empty!");
         return;
     }
+
     if (inputAddTask.value[0] === ' ') {
         alert("Task value must start with non-SPACE symbol!");
         inputAddTask.value = '';
         return;
     }
+
     todoListArr.push(newTodo);
-    renderMessages();
-    inputAddTask.value = '';
-}
+    localStorage.setItem('todo', JSON.stringify(todoListArr));
+    reloadRender();
+    renderTasks();
+    console.log(localStorage.getItem('todo'));
+};
 
-const renderMessages = () => {
-    let renderMessage = '';
-    todoListArr.forEach((item, i) => {
-        renderMessage += `
-            <li class=" ${item.checked ? ' checked' : ''}">
-                <input 
-                
-                type="checkbox" 
-                id="item_${++i}" 
-                ${item.checked ? 'checked' : ''}
-                >
-                <label
-                 class="tasks-list-item ${item.important ? ' important' : ''}"
-                 for="item_${++i}"
-                 
-                 >${item.todo}</label>
-            </li>
-     `;
-        tasksList.innerHTML = renderMessage;
-    })
-}
 
-renderMessages();
+document.addEventListener("DOMContentLoaded", renderTasks);
 
 buttonAddTask.addEventListener('click', buttonAddTaskHandler);
+
+tasksList.addEventListener('change', event => {
+    let idInput = event.target.getAttribute('id');
+    let forLabel = tasksList.querySelector('[for="' + idInput + '"]');
+    let valueLabel = forLabel.innerHTML;
+    todoListArr.forEach((item) => {
+        if (item.todo === valueLabel) {
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoListArr));
+        }
+    })
+});
+
+tasksList.addEventListener('contextmenu', event => {
+    event.preventDefault();
+    todoListArr.forEach((item) => {
+        if (item.todo === event.target.innerHTML) {
+            item.important = !item.important;
+            localStorage.setItem('todo', JSON.stringify(todoListArr));
+        }
+
+    })
+    reloadRender();
+});
